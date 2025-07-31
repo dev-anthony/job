4e5c34ab212db0f3987746ce7b4ea13c5b8ea937
Anthony Achibi <anthonyachibi@gmail.com>
2025-07-31 22:14:53 +0100

    _

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import { JobProvider } from './contexts/JobContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <JobProvider>
       <App />
     </JobProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
